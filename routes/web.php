b77e49ee804a857168c34776227bf9d12eb89511
kimhao97 <nguyenkimhaodn@gmail.com>
2024-04-17 15:14:58 +0700

    refactor: remname idea

--- routes/web.php
+++ routes/web.php
@@ -5,8 +5,8 @@ use App\Http\Controllers\IdeaController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', [DashboardController::class, 'index'])->name('dashboard');
-Route::post('/idea', [IdeaController::class, 'store'] )->name('idea.create');
-Route::delete('/idea/{idea}', [IdeaController::class, 'destroy'] )->name('ideas.destroy');
+Route::post('/ideas', [IdeaController::class, 'store'] )->name('ideas.create');
+Route::delete('/ideas/{idea}', [IdeaController::class, 'destroy'] )->name('ideas.destroy');
 
 Route::get('/terms', function () {
     return view('terms');
