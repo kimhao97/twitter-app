592fa6269bfdc5c134f0e16a12fd08e3609c5060
kimhao97 <nguyenkimhaodn@gmail.com>
2024-04-20 14:45:10 +0700

    authorization: eloquent relationships

--- app/Models/Idea.php
+++ app/Models/Idea.php
@@ -18,4 +18,8 @@ class Idea extends Model
     public function comments() {
         return $this->hasMany(Comment::class);
     }
+
+    public function user() {
+        return $this->belongsTo(User::class);
+    }
 }
